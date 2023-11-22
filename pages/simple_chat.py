cc8304868feaf98dbe89faa7fffa140744f806bd
Yuto Urashima <yuto.urashima24@gmail.com>
2023-11-22 14:03:10 +0000

    use chat completion api

--- pages/simple_chat.py
+++ pages/simple_chat.py
@@ -1,9 +1,17 @@
 import streamlit as st
+import openai
 
 st.title("simple chat")
 
 user_message = st.text_input(label="どうしましたか？")
 
 if user_message:
-    ai_message = "こんにちは！"
-    st.write(ai_message)
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        temperature=0,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": user_message},
+        ],
+    )
+    st.write(completion)
